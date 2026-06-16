ffdd56b8ea485ea67bfd300138dcc2c1adf3f9a8
Sai Prajoth <saiprajoth@gmail.com>
2026-06-16 21:25:49 +0530

    backend-url issue fixed

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", // Must be a specific URL (Cannot be "*")
+  origin: process.env.FRONTEND_URL, // Must be a specific URL (Cannot be "*")
   credentials: true                // Allows cookies to travel across origins
 }));
 
